61699501b789e0e6274f7acabc1de6a5839c3564
Laurent Grangeau <laurent.grangeau@gmail.com>
2021-10-03 21:22:17 +0200

    Add provider and version
    Signed-off-by: Laurent Grangeau <laurent.grangeau@gmail.com>

--- answer/iaas/provider.tf
+++ answer/iaas/provider.tf
@@ -1,6 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.61.0"
+    }
+  }
+}
+
 provider "aws" {
   region  = "eu-west-1" # You can put either 'eu-west-1' or 'eu-central-1'
-  access_key = ""
-  secret_key = ""
-  token = ""
 }
