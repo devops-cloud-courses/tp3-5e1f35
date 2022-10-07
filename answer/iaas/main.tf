55bbbb884691e1ce061614095ef5b5c62aff882f
Laurent Grangeau <laurent.grangeau@gmail.com>
2022-10-07 14:25:11 +0200

    Scaffold Terraform files
    Signed-off-by: Laurent Grangeau <laurent.grangeau@gmail.com>

---
+++ answer/iaas/main.tf
@@ -0,0 +1,53 @@
+resource "aws_instance" "my_instance" {
+  ### EDIT HERE !
+
+  ### DO NOT EDIT BELOW !!
+  count         = 1
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+}
+
+resource "aws_key_pair" "my_kp" {
+  ### EDIT HERE !
+}
+
+resource "aws_vpc" "my_vpc" {
+  ### EDIT HERE !
+}
+
+resource "aws_subnet" "my_sn" {
+  ### EDIT HERE !
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  ### EDIT HERE !
+}
+
+resource "aws_route_table" "my_rt" {
+  ### EDIT HERE !
+}
+
+resource "aws_route_table_association" "my_rta" {
+  ### EDIT HERE !
+}
+
+resource "aws_security_group" "my_sg" {
+  ### EDIT HERE !
+}
+
+### DO NOT EDIT BELOW !!
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
---
+++ answer/paas/main.tf
@@ -0,0 +1,15 @@
+resource "aws_elastic_beanstalk_environment" "my_bs_env" {
+  ### EDIT HERE !
+}
+
+resource "aws_elastic_beanstalk_application" "my_bs_app" {
+  ### EDIT HERE !
+}
+
+resource "aws_elastic_beanstalk_configuration_template" "my_bs_ct" {
+  ### EDIT HERE !
+}
+
+resource "aws_db_instance" "my_db_instance" {
+  ### EDIT HERE !
+}
